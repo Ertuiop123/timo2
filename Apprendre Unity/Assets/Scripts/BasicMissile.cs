5ae790e4c93bfb6b8a5042a8852a161d4e81cfa8
Lebgdu64 <108421696+Lebgdu64@users.noreply.github.com>
2022-11-19 22:53:49 -0500

    disable projectile

--- Apprendre Unity/Assets/Scripts/BasicMissile.cs
+++ Apprendre Unity/Assets/Scripts/BasicMissile.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BasicMissile : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
+    public float lifeTime = 3f;
 
     private void Start() {
         rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+
+        StartCoroutine(WaitThenDie());
+    }
+    IEnumerator WaitThenDie()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -19,4 +28,6 @@ public class BasicMissile : MonoBehaviour
 
         }
     }
+    
+
 }
