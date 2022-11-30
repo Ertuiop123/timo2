ff78174df58561c9abdb86e01cef58272d344d3c
Lebgdu64 <108421696+Lebgdu64@users.noreply.github.com>
2022-11-29 20:23:51 -0500

    Revert "Revert "shop""
    This reverts commit faae6d4fdc83abcd2471dd8330b48cdb86f105da.

--- Apprendre Unity/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Apprendre Unity/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour {
     Vector2 movements;
     Vector2 referenceVelocity;
     public ContactFilter2D mask;
+    public GameObject shop;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -29,24 +30,31 @@ public class PlayerMovement : MonoBehaviour {
 
     }
 
-    void FixedUpdate() {
-        Move();
-    }
-
     void GetInputs() {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+
 
         animationState = x;
 
         gameObject.SetActive(!gameObject.activeSelf);
 
         movements = new Vector2(x, y).normalized * Time.fixedDeltaTime * speed * 100;
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            shop.SetActive(!shop.activeSelf);
+        }
+
     }
 
     void Move() {
         Vector2 targetVelocity = movements;
 
         rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref referenceVelocity, smoothMovementIntensity);
+    }
+    void FixedUpdate()
+    {
+        Move();
     }
 }
